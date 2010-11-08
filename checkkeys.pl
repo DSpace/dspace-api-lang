7c5a526f8393e3b3d34d1e42acc0eb9a11efad0c
mdiggory <mdiggory@atmire.com>
2010-11-08 19:58:50 +0000

    Update Languages to support dspace-pom and have correct license formatting.

--- checkkeys.pl
+++ checkkeys.pl
@@ -1,4 +1,11 @@
 #!/usr/bin/perl -w
+#
+# The contents of this file are subject to the license and copyright
+# detailed in the LICENSE and NOTICE files at the root of the source
+# tree and available online at
+#
+# http://www.dspace.org/license/
+#
 
 ###########################################################################
 #
